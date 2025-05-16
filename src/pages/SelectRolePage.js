7c68eb834ad9f9db03bb04e75025eca9adb5d210
reyhaonan <reyhaonan2003@gmail.com>
2025-05-16 21:54:07 +0700

    chore: select role page

---
+++ src/pages/SelectRolePage.js
@@ -0,0 +1,5 @@
+const SelectRolePage = () => {
+    return <>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ab ad adipisci consequuntur culpa doloremque eius illum iusto, labore minus, modi natus officia placeat quas rem repudiandae saepe vitae? Autem corporis cupiditate, doloribus excepturi expedita modi nisi omnis reprehenderit ut vitae?</>
+}
+
+export default SelectRolePage;
